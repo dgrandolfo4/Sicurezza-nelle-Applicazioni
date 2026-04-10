f5af1f3a3d68b2970371ba30660af3762907d916
dgrandolfo <danielegrandolfo@gmail.com>
2026-04-10 21:13:10 +0200

    Rimosso codice commentato

--- src/main/java/uniba/sna/dao/RegistrazioneDAO.java
+++ src/main/java/uniba/sna/dao/RegistrazioneDAO.java
@@ -14,71 +14,7 @@ public class RegistrazioneDAO extends DatabaseDAO {
     /**
      * Registra un nuovo utente e salva le informazioni della sua foto profilo.
      */
-	/* public boolean registerUser(String email, byte[] hashedPassword, String salt, String originalName, String uniqueName) {
-        int generatedUserId = -1;
-        boolean isSuccess = false;
-
-        try {
-            if (connect()) {
-                String sqlUser = AppProperties.getQueryProperty("insertAppUserQuery");
-                PreparedStatement psUser = connessione.prepareStatement(sqlUser, Statement.RETURN_GENERATED_KEYS);
-                psUser.setString(1, email);
-                int rowsAffected = psUser.executeUpdate();
-                if (rowsAffected > 0) {
-                    ResultSet rs = psUser.getGeneratedKeys();
-                    if (rs.next()) {
-                    	generatedUserId = rs.getInt(1);
-                    }
-                }
-            }
-        } catch (Exception e) {
-        	e.printStackTrace(); 
-        	return false; 
-    	} 
-        finally { 
-        	close(); 
-    	}
-
-        if (generatedUserId == -1) return false;
-
-        try {
-            if (connect(true)) { // DB Auth
-            	String base64Password = Base64.getEncoder().encodeToString(hashedPassword);
-
-                String sqlAuth = AppProperties.getQueryProperty("insertAuthUserQuery");
-                PreparedStatement psAuth = connessione.prepareStatement(sqlAuth);
-                psAuth.setInt(1, generatedUserId);
-                psAuth.setString(2, base64Password);
-                psAuth.setString(3, salt);
-                if (psAuth.executeUpdate() == 0) return false;
-            }
-        } catch (Exception e) { 
-        	e.printStackTrace(); 
-        	return false; 
-    	} 
-        finally { 
-        	close(); 
-    	}
-
-        // FASE 3: Salvataggio File Profilo su DB App
-        try {
-            if (connect(false)) { // DB App
-                String sqlFile = AppProperties.getQueryProperty("insertFileQuery");
-                try (PreparedStatement psFile = connessione.prepareStatement(sqlFile)) {
-                    psFile.setString(1, originalName);
-                    psFile.setString(2, uniqueName);
-                    psFile.setInt(3, 1); // 1 = Categoria Immagine Profilo
-                    psFile.setInt(4, generatedUserId);
-                    if (psFile.executeUpdate() > 0) isSuccess = true;
-                }
-            }
-        } catch (Exception e) { e.printStackTrace(); } 
-        finally { close(); }
-
-        return isSuccess;
-    } */
-	
-    public boolean registerUser(String email, 
+	public boolean registerUser(String email, 
     							byte[] hashedPassword, 
 								byte[] salt,
                                 String fileName,
